ae16d976e81fec8eb0297002222f75234ec46c7f
luanrsiqueira <luanrsiqueira18@gmail.com>
2021-07-15 18:59:01 -0300

    Files

---
+++ 24-06-teste.py
@@ -0,0 +1,82 @@
+import getopt
+import sys
+import subprocess
+import os
+import csv
+import glob 
+import pandas as pd
+from pathlib import Path
+
+encoding = 'utf-8'
+
+def Convert(string):
+    li = list(string.split("\n"))
+    return li
+
+if not os.path.exists(f"/home/luan/projeto_Tcc/cuda_to_csv"):
+    new_folder = os.mkdir(f"/home/luan/projeto_Tcc/cuda_to_csv")
+else:
+    pass
+
+for file in glob.glob("/home/luan/projeto_Tcc/cuda_files/*.csv"):
+    finfo = Path(file)
+    explod_file = finfo.name.split('.')
+    title_file = '.'.join(explod_file[:-1])
+    title_file = Convert(title_file)[0]
+
+    architecture = "cat " + file + " | grep sm_ | awk -F'[^0-9]*' '{print $3}'"
+    tt = subprocess.Popen(architecture, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    tt = tt.decode(encoding)
+    out_architecture = tt.rstrip('\n')
+    out_architecture = Convert(out_architecture)
+
+    tw = "cat " + file + " | grep Tile_Width | awk -F'[^0-9]*' '{print $2}'"
+    out_tw = subprocess.Popen(tw, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    out_tw = out_tw.decode(encoding)
+    out_tile_width = out_tw.rstrip('\n')
+    out_tile_width = Convert(out_tile_width)
+    #print(out_tile_width)
+
+    if out_architecture == ['0', '0', '0','0', '0', '0','0', '0', '0']:
+        architecture = "cat " + file + " | grep sm_ | awk  -F'[^0-9]*' '{print $4}'"
+        tt = subprocess.Popen(architecture, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        tt = tt.decode(encoding)
+        out_architecture = tt.rstrip('\n')
+        out_architecture = Convert(out_architecture)
+    else:
+        pass
+
+    run1 = "cat /home/luan/projeto_Tcc/cuda_files/"+title_file+".csv | grep registers | awk -F'[^0-9]*' '{print $2}'"
+    out1 = subprocess.Popen(run1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    out1 = out1.decode(encoding)
+    out_registers = out1.rstrip('\n')
+    out_registers = Convert(out_registers)
+
+    run2 = "cat /home/luan/projeto_Tcc/cuda_files/"+title_file+".csv | grep smem | awk -F'[^0-9]*' '{print $3}'"
+    out2 = subprocess.Popen(run2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    out2 = out2.decode(encoding)
+    out_smem = out2.rstrip('\n')
+    out_smem = Convert(out_smem)
+
+    if out_smem == ['']:
+        out_smem = '0'
+        run_again = "cat /home/luan/projeto_Tcc/cuda_files/"+title_file+".csv | grep cmem | awk -F'[^0-9]*' '{print $3}'"
+        out4 = subprocess.Popen(run_again, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        out4 = out4.decode(encoding)
+        out_cmem_1 = out4.rstrip('\n')
+        out_cmem = out_cmem_1
+        out_cmem = Convert(out_cmem)
+        dict = {'Algorithm': title_file, 'Tile_Width':out_tile_width, 'Architecture': out_architecture, 'Registers': out_registers, 'smem': out_smem, 'cmem' : out_cmem } 
+        df = pd.DataFrame(dict) 
+        df.to_csv('./cuda_to_csv/output.csv', mode='a', header= False)
+ 
+    else:
+        run3 = "cat /home/luan/projeto_Tcc/cuda_files/"+title_file+".csv | grep cmem | awk -F'[^0-9]*' '{print $4}'"
+        out3 = subprocess.Popen(run3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        out3 = out3.decode(encoding)
+        out_cmem = out3.rstrip('\n')
+        out_cmem = Convert(out_cmem)
+ 
+        dict = {'Algorithm': title_file, 'Tile_Width':out_tile_width, 'Architecture': out_architecture, 'Registers': out_registers, 'smem': out_smem, 'cmem' : out_cmem }
+        df = pd.DataFrame(dict) 
+        df.to_csv('./cuda_to_csv/output.csv', mode='a', header= False)
---
+++ run_Cuda.py
@@ -0,0 +1,35 @@
+import getopt
+import sys
+import subprocess
+import os
+import glob
+from pathlib import Path
+
+architecture = ['30', '35', '52']
+#tile_width = ['8', '16', '32']
+
+if not os.path.exists(f"/home/luan/projeto_Tcc/cuda_files"):
+    new_folder = os.mkdir(f"/home/luan/projeto_Tcc/cuda_files")
+else:
+    pass
+
+for file in glob.glob("cuda_analysis/src/files/*.cu"):
+    finfo = Path(file)
+    explod_file = finfo.name.split('.')
+    title_file = '.'.join(explod_file[:-1])
+    title = f"echo Name: {title_file} >>  ./cuda_files/{title_file}.csv 2>&1"
+
+    architecture = ['30', '35', '52']
+
+    if title_file == 'subSeqMax':
+        tile_width = ['128']
+    else:
+        tile_width = ['8', '16', '32']
+    for row in architecture:
+        for line in tile_width:
+            tw = f"echo Tile_Width: {line} >> ./cuda_files/{title_file}.csv 2>&1"
+            run_script = f"nvcc -D Tile_Width={line} -arch=sm_{row} -Xptxas=-v {file} -w >> ./cuda_files/{title_file}.csv 2>&1"
+            subprocess.call(title, shell=True)
+            subprocess.call(tw, shell=True)
+            subprocess.call(run_script, shell=True)
+print("Complete!")
